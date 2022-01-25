e3addb465580bf31a09c34de92f908b110d857af
Julius Franklin <jjfranklin88@outlook.com>
2022-01-25 15:41:11 -0500

    adding test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const PictureServices = require('../lib/services/PictureServices');
 
 describe('backend routes', () => {
   beforeEach(() => {
@@ -10,5 +11,16 @@ describe('backend routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should add user and image', async () => {
+    const res = await PictureServices.createUser(
+      'julius');
+
+    expect(res).toEqual({
+      id: expect.any(String),
+      user_name: 'julius',
+      img_url: 'https://www.youtube.com/embed/s6IpsM_HNcU?rel=0',
+    });
   });
 });
